30401aa39e1d0f2d91fb63beb709f0fe91081736
jguilhermecoelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2021-01-23 15:15:46 -0300

    desafio final nodejs

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -10,6 +10,7 @@ api.findAll = (request, response) => {
       response.status(exception.status | 400);
       response.json({ mensagem: setence });
     }
+    response.status(201);
     response.json(cards);
   });
 };
@@ -40,22 +41,47 @@ api.find = (request, response) => {
         response.status(exception.status | 400);
         response.json({ mensagem: setence });
       }
+      response.status(201);
       response.json(card);
     });
 };
 
 api.remove = (request, response) => {
-  neDB.remove({_id: parseInt(request.params.id)}, {}, (exception, cardRemoved) => {
+  neDB.remove(
+    { _id: parseInt(request.params.id) },
+    {},
+    (exception, cardRemoved) => {
       if (exception) {
         const setence = "It was not possible to remove the card!";
         console.log(setence, exception);
         response.status(exception.status | 400);
         response.json({ mensagem: setence });
       }
-      response.status(200);
+      response.status(201);
       response.json(cardRemoved);
-  });
+    }
+  );
 };
 
+api.update = (request, response) => {
+  const canonical = request.body;
+
+  neDB.update(
+    { _id: parseInt(request.params.id) },
+    canonical,
+    {},
+    (exception, cardUpdated) => {
+      if (exception) {
+        const sentence = "It was not possible to update the card!";
+        console.log(sentence, exception);
+
+        response.status(exception.status | 400);
+        response.json({ message: sentence });
+      }
+      response.status(201);
+      response.json(cardUpdated);
+    }
+  );
+};
 
 module.exports = api;
--- nodejs/cards/src/routes/card-route.js
+++ nodejs/cards/src/routes/card-route.js
@@ -5,4 +5,5 @@ module.exports = (app) => {
   app.get("/cards/:id", api.find);
   app.post("/cards", api.save);
   app.delete("/cards/:id", api.remove);
+  app.put("/cards/:id", api.update)
 };
